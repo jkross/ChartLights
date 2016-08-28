c21a2861e3180b62eb6e770a41f4e4cc72cd9ad9
Jonathan Ross <j.k.ross@outlook.com>
2016-08-28 14:48:14 -0700

    initial version

---
+++ ChartLights/SimpleSerial2/SimpleSerial2.ino
@@ -0,0 +1,65 @@
+// Cascaded TLC5916 drivers
+const int latchPin = 12;  // connected to LE  of TLC5916
+const int clockPin = 8;   // connected to CLK of TLC5916
+const int dataPin = 11;   // connected to SDI of TLC5916
+const int oePin = 3;      // connected to ~OE of TLC5916
+
+// Since ~OE is active low we calcuate our PWM percent as the inverse %
+#define PWM_PCT_ACT_LOW(_PCT) ((int)255*(1-(_PCT/100.0)))
+#define PWM_PCT 10
+
+#define NUM_BANKS 2       // number of drivers
+#define LEDS_PER_BANK 8
+byte datArray[][NUM_BANKS] = {{0x0, 0x0},
+                              {0x1, 0x0},
+                              {0x1, 0x1},
+                              };
+#define NUM_SAMPLES (sizeof(datArray)/sizeof(datArray[0]))
+
+// Timing delay for test loop
+#define DELAY 5000
+
+void setup ()
+{
+  //set pins to output
+  pinMode(latchPin,OUTPUT);
+  pinMode(clockPin,OUTPUT);
+  pinMode(dataPin,OUTPUT);
+  pinMode(oePin,OUTPUT);
+  Serial.begin(57600);
+  digitalWrite(latchPin,LOW);
+  analogWrite(oePin, PWM_PCT_ACT_LOW(PWM_PCT));
+}
+
+void loop()
+{
+  //loop through all samples
+  for(int sample = 0; sample < NUM_SAMPLES; sample++)
+  {
+      for (int rpt = 0; rpt < 10; rpt++) {
+        //digitalWrite(oePin,HIGH); // disable LEDs and don't let driver switch to non-standard mode
+        for (int bank = 1; bank >= 0; bank--)
+        {
+          //dPrint(sample, bank);
+          shiftOut(dataPin,clockPin,MSBFIRST,datArray[sample][bank]);
+        }
+        // toggle latch pin to latch data from shift register to output drivers
+        digitalWrite(latchPin,HIGH); 
+        digitalWrite(latchPin,LOW); 
+        // set ~OE according to PWM percentage
+        //analogWrite(oePin, PWM_PCT_ACT_LOW(PWM_PCT));
+        delay(DELAY/10);
+    }
+  }
+}
+
+void dPrint(int sample, int bank)
+{
+      Serial.print("sample: ");
+      Serial.print(sample, DEC);
+      Serial.print(" bank: ");
+      Serial.print(bank, DEC);
+      Serial.print(" ");
+      Serial.print(datArray[sample][bank],DEC);
+      Serial.println();
+}
