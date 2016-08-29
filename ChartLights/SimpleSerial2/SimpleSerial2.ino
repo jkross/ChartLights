960ad0fc581c72b5f53b428db707d11ea8b31bf8
Jonathan Ross <j.k.ross@outlook.com>
2016-08-28 18:15:33 -0700

    refactored into separate file

--- ChartLights/SimpleSerial2/SimpleSerial2.ino
+++ ChartLights/SimpleSerial2/SimpleSerial2.ino
@@ -1,4 +1,7 @@
 // Cascaded TLC5916 drivers
+
+#include "perfStat.h"
+
 const int latchPin = 12;  // connected to LE  of TLC5916
 const int clockPin = 8;   // connected to CLK of TLC5916
 const int dataPin = 11;   // connected to SDI of TLC5916
@@ -11,55 +14,66 @@ const int oePin = 3;      // connected to ~OE of TLC5916
 #define NUM_BANKS 2       // number of drivers
 #define LEDS_PER_BANK 8
 byte datArray[][NUM_BANKS] = {{0x0, 0x0},
-                              {0x1, 0x0},
-                              {0x1, 0x1},
-                              };
+  {0x1, 0x0},
+  {0x1, 0x1},
+};
 #define NUM_SAMPLES (sizeof(datArray)/sizeof(datArray[0]))
 
 // Timing delay for test loop
-#define DELAY 5000
+#define DELAY 1000
+
+
+perfStat<100> *cnt;
 
 void setup ()
 {
   //set pins to output
-  pinMode(latchPin,OUTPUT);
-  pinMode(clockPin,OUTPUT);
-  pinMode(dataPin,OUTPUT);
-  pinMode(oePin,OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(oePin, OUTPUT);
   Serial.begin(57600);
-  digitalWrite(latchPin,LOW);
+  digitalWrite(latchPin, LOW);
   analogWrite(oePin, PWM_PCT_ACT_LOW(PWM_PCT));
+  cnt = new perfStat<100>();
 }
 
 void loop()
 {
   //loop through all samples
-  for(int sample = 0; sample < NUM_SAMPLES; sample++)
+  for (int sample = 0; sample < NUM_SAMPLES; sample++)
   {
-      for (int rpt = 0; rpt < 10; rpt++) {
+    {
+// new variable through the loop hangs after 3 iterations - looks like memory isn't being freed
+//      auto cnt = new perfStat<100>();
+      cnt->init();
+      do {
+        cnt->start();
         //digitalWrite(oePin,HIGH); // disable LEDs and don't let driver switch to non-standard mode
         for (int bank = 1; bank >= 0; bank--)
         {
           //dPrint(sample, bank);
-          shiftOut(dataPin,clockPin,MSBFIRST,datArray[sample][bank]);
+          shiftOut(dataPin, clockPin, MSBFIRST, datArray[sample][bank]);
         }
         // toggle latch pin to latch data from shift register to output drivers
-        digitalWrite(latchPin,HIGH); 
-        digitalWrite(latchPin,LOW); 
+        digitalWrite(latchPin, HIGH);
+        digitalWrite(latchPin, LOW);
         // set ~OE according to PWM percentage
         //analogWrite(oePin, PWM_PCT_ACT_LOW(PWM_PCT));
-        delay(DELAY/10);
+      } while (!(cnt->stop()));
+      cnt->compute();
     }
+    delay(DELAY);
   }
 }
 
 void dPrint(int sample, int bank)
 {
-      Serial.print("sample: ");
-      Serial.print(sample, DEC);
-      Serial.print(" bank: ");
-      Serial.print(bank, DEC);
-      Serial.print(" ");
-      Serial.print(datArray[sample][bank],DEC);
-      Serial.println();
+  Serial.print("sample: ");
+  Serial.print(sample, DEC);
+  Serial.print(" bank: ");
+  Serial.print(bank, DEC);
+  Serial.print(" ");
+  Serial.print(datArray[sample][bank], DEC);
+  Serial.println();
 }
--- ChartLights/SimpleSerial2/perfStat.h
+++ ChartLights/SimpleSerial2/perfStat.h
@@ -5,29 +5,52 @@
 
 template <int COUNT>
 class perfStat {
-private:
+  private:
     typedef unsigned long sample_t;
     sample_t  _stats[COUNT];
     sample_t  _start;
     int       _index;
 
-public:
-    perfStat() : _index(0)
+  public:
+    perfStat()
     {
-      for (int i=0; i < COUNT; i++)
+      init();
+    }
+
+    void init()
+    {
+      _index = 0;
+      for (int i = 0; i < COUNT; i++)
       {
         _stats[i] = 0;
       }
     }
 
-    ~perfStat()
-    {
-      compute();
-    }
+// Defining this destructor has problems if there are local variable of this class type
+//    ~perfStat()
+//    {
+//      compute();
+//    }
 
     void compute()
     {
-      Serial.println("Stats Printed");
+      sample_t min = ~((sample_t)0), max = 0, sum = 0;
+      for (int i = 0; i < COUNT; i++) {
+        sample_t s = _stats[i];
+        sum += s;
+        min = s < min ? s : min;
+        max = s > max ? s : max;
+      }
+      double avg = (double)sum / COUNT;
+      Serial.print("Avg: ");
+      Serial.print(avg, 2);
+      Serial.print(" Min: ");
+      Serial.print(min, DEC);
+      Serial.print(" Max: ");
+      Serial.print(max, DEC);
+      Serial.print(" Count: ");
+      Serial.print(COUNT, DEC);
+      Serial.println();
     }
 
     void start()
@@ -37,9 +60,12 @@ public:
 
     bool stop()
     {
-      sample_t now = micros();
-      _stats[_index++] = now - _start;
-      return (_index < COUNT);
+      if (_index < COUNT) {
+        sample_t now = micros();
+        _stats[_index++] = now - _start;
+      }
+      return (_index >= COUNT);
     }
 };
+
 #endif
